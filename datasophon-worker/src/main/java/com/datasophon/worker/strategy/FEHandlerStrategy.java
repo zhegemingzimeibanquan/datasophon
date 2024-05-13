57846702618d48f117388b568745a618a3796b49
88fantasy <88fantasy@gmail.com>
2024-05-13 23:45:04 +0800

    [bugfix][worker] doris 增加节点时采用ip (#552)
    应当使用IP,否则应用侧使用时需要设置host

--- datasophon-worker/src/main/java/com/datasophon/worker/strategy/FEHandlerStrategy.java
+++ datasophon-worker/src/main/java/com/datasophon/worker/strategy/FEHandlerStrategy.java
@@ -18,6 +18,7 @@
 package com.datasophon.worker.strategy;
 
 import akka.actor.ActorRef;
+import cn.hutool.core.net.NetUtil;
 import cn.hutool.json.JSONUtil;
 import com.datasophon.common.Constants;
 import com.datasophon.common.cache.CacheUtils;
@@ -63,7 +64,7 @@ public class FEHandlerStrategy extends AbstractHandlerStrategy implements Servic
                     try {
                         OlapSqlExecCommand sqlExecCommand = new OlapSqlExecCommand();
                         sqlExecCommand.setFeMaster(command.getMasterHost());
-                        sqlExecCommand.setHostName(CacheUtils.getString(Constants.HOSTNAME));
+                        sqlExecCommand.setHostName(NetUtil.getLocalhostStr());
                         sqlExecCommand.setOpsType(OlapOpsType.ADD_FE_FOLLOWER);
                         ActorUtils.getRemoteActor(command.getManagerHost(), "masterNodeProcessingActor")
                                 .tell(sqlExecCommand, ActorRef.noSender());
